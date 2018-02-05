b4fb0852ac285018285190cbdc805ea093254bd1
Paul Holden <pdmholden@gmail.com>
2018-02-04 17:40:44 -0800

    Basic class & testing structure

---
+++ extract_winner_spec.rb
@@ -0,0 +1,13 @@
+require 'extract_winner'
+
+describe 'ExtractWinner' do
+  context 'simple cases' do
+    context 'when the string is too short' do
+      it "doesn't provide numbers" do
+      end
+    end
+  end
+
+  context 'provided cases' do
+  end
+end
---
+++ lib/extract_winner.rb
@@ -0,0 +1,2 @@
+class ExtractWinner
+end
