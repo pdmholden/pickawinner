e5ad79e42e357cbcc9be6594078f2569bbaa3f78
Paul Holden <pdmholden@gmail.com>
2018-02-06 21:00:03 -0800

    Flesh out methods and some specs

--- extract_winner_spec.rb
+++ extract_winner_spec.rb
@@ -1,13 +1,25 @@
 require 'extract_winner'
 
 describe 'ExtractWinner' do
-  context 'simple cases' do
-    context 'when the string is too short' do
-      it "doesn't provide numbers" do
-      end
+  describe '#print_picks' do
+  end
+
+  describe '#pick' do
+    let(:service) do
+      ExtractWinner.new(["569815571556",
+        "4938532894754",
+        "1234567",
+        "472844278465445"])
+    end
+
+    it 'returns array of same length' do
+      result = service.pick
+      expect(result.length).to eq 4
     end
   end
 
-  context 'provided cases' do
+  describe '#process_input' do
+    describe 'inputs provided' do
+    end
   end
 end
--- lib/extract_winner.rb
+++ lib/extract_winner.rb
@@ -1,2 +1,27 @@
 class ExtractWinner
+
+  def initialize(strings)
+    @inputs = strings
+    @outputs = []
+  end
+
+  def print_picks
+  end
+
+  def pick
+    @inputs.each do |str|
+      @outputs << process_input(str)
+    end
+
+    @outputs
+  end
+
+  def process_input(item)
+    return nil if item.length < 7
+    return nil if item.length > 14
+    return nil unless (item =~ /[[:alpha:]]/).nil?
+
+    possibilities = []
+  end
+
 end
